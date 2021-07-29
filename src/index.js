3537ccabb0fd8c6fad00d2f389e5126531243fec
Dmytro Shcherba <dscherba76@gmail.com>
2021-07-29 14:17:52 +0300

    lesson1 homework done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
 
-const myName = 'Dmytro';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={myName} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
