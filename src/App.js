a622f0140e6b25cdc6f68db9086b9a877206dedb
Dmytro Shcherba <dscherba76@gmail.com>
2021-08-09 16:11:01 +0300

    added useStyles hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "normalize.css"
 import { Grid, makeStyles } from "@material-ui/core"
-import styles from "./App.module.sass"
 import { ChatList } from "./components/chat-list"
 import { MessageList } from "./components/message-list"
 
+const useStyles = makeStyles({
+  app: {
+    fontFamily: "Roboto, Helvetica, sans-serif",
+  },
+})
+
 export function App() {
+  const classes = useStyles()
   return (
-    <div className={styles.app}>
+    <div className={classes.app}>
       <Grid container={true}>
         <Grid item={true} xs={12} md={3}>
           <ChatList />
