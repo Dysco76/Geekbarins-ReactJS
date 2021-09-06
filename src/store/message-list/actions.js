f766c2a6fc00fbf24515bd97df8010a35b6a3027
Dmytro Shcherba <dscherba76@gmail.com>
2021-09-06 17:35:33 +0300

    Update actions.js

--- src/store/message-list/actions.js
+++ src/store/message-list/actions.js
@@ -1,22 +1,6 @@
-import { clearMessageInput } from "../conversations-list"
 import { ADD_MESSAGE } from "./types"
 
 export const addMessage = (message, roomId) => ({
   type: ADD_MESSAGE,
   payload: { message, roomId },
 })
-
-export const sendMessageThunk = (message, roomId) => (dispatch, getState) => {
-  dispatch(addMessage(message, roomId))
-  dispatch(clearMessageInput(roomId))
-
-  if (message.author === getState().profile.user.name) {
-    setTimeout(
-      () =>
-        dispatch(
-          addMessage({ author: "bot", message: "Hello from bot!" }, roomId),
-        ),
-      500,
-    )
-  }
-}
