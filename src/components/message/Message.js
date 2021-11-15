a7e2bd835927af80dca016c7a36b902f638ccef5
Dmytro Shcherba <dscherba76@gmail.com>
2021-11-15 21:05:12 +0200

    fixed context action duplicating after messages edits

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -22,6 +22,8 @@ export function Message({
 
   // set context actions depending if the user is the author or the admin
   useEffect(() => {
+    // remove existing context actions on re-render
+    setContextActions([])
     if (authorId === auth.currentUser.uid) {
       setContextActions((prevState) => [
         ...prevState,
